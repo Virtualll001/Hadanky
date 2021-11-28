bd5622ae1274974ab630e5bed4ec3d60bf47ba7f
Kateřina Mlázovská <60098902+Virtualll001@users.noreply.github.com>
2021-11-28 15:37:28 +0100

    28.11.2021

--- HadankyScaffolded/Startup.cs
+++ HadankyScaffolded/Startup.cs
@@ -32,8 +32,8 @@ namespace HadankyScaffolded
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddEntityFrameworkStores<ApplicationDbContext>(); 
             services.AddControllersWithViews();
         }
 
